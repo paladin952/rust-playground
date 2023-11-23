89d1948d5f35185fd837338054e769b8bf9f2009
Lucian Clapa <clapalucian10@gmail.com>
2023-11-23 23:59:59 +0200

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,35 @@
+fn add(a: i32, b: i32) -> i32 {
+    a + b
+}
+
+fn main() {
+    let x = 5;
+
+    println!("The value of x is: {}", x);
+    println!("Hello, world!");
+
+    println!("{a}, {b}", a = "Hello2", b = "world2!");
+
+    let mut sum = 0;
+
+    for i in 0..5 {
+        sum += i;
+        let text = if i % 2 == 0 { "even" } else { "odd" };
+
+        println!("Hello, world! {} {}", i, text);
+    }
+
+    println!("sum is {}", sum);
+
+    let a: i32 = add(20, 20);
+    let b: i32 = add(20, 30);
+
+    println!("a is {}", a);
+    println!("b is {}", b);
+
+    let i = 10;
+
+    let i_ref = &i;
+    println!("i ref is {}", i_ref);
+    println!("i ref derrefed is {}", *i_ref);
+}
